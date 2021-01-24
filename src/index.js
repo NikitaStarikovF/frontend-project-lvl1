20354f0372a092cb2f91a0225187116edb85d7c3
Nikita Starikov <december_soul@inbox.ru>
2021-01-25 00:41:45 +0300

    final 1.3

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ const startEngine = (brifing, getGameData) => {
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Answer: ');
     if (answer !== correctAnswer) {
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}!`);
+      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\n Let's try again, ${userName}!`);
+
       return;
     }
     console.log('Correct!');
